f8d2432853d495e4a21c31ba2b09d186d369bf5e
bausano <gtasampserver@seznam.cz>
2017-09-04 23:07:02 +0100

    #5

---
+++ js/menu.js
@@ -0,0 +1,40 @@
+const Bookmarks = chrome.bookmarks
+const Folder = 'Linkgroups-extension'
+const Menu = chrome.contextMenus
+
+var save = (info, tab) => {
+  Bookmarks.create({
+    title: tab.title,
+    url: tab.url,
+    parentId: info.menuItemId
+  })
+}
+
+function reloadMenu()
+{
+  Menu.removeAll()
+
+  Bookmarks.search({title: Folder}, (root) => {
+    if (root.length === 0) {
+      return false
+    }
+    Bookmarks.getChildren(root[0].id, (groups) => {
+      groups.forEach((group) => {
+        Menu.create({
+          id: group.id,
+          title: group.title,
+          contexts: ['all'],
+          onclick: save
+        })
+      })
+    })
+  })
+}
+
+chrome.extension.onMessage.addListener((request) => {
+  if( request.menu === "update" ) {
+    reloadMenu()
+  }
+})
+
+reloadMenu()
